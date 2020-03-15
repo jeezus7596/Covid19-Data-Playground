7ad1a053c9578847bb9c607cbcf59c9c71b6dabc
jeezus <tri.utkarsh7596@gmail.com>
2020-03-15 16:05:42 +0530

    Realign xaxis to origin

--- curveFit.m
+++ curveFit.m
@@ -7,7 +7,7 @@ indiaCases = data(1,1:end-1);
 
 % Some vector definitions
 Ndays = 60;
-dayVec = 1:Ndays;
+dayVec = 0:Ndays;
 startDate = datetime(2020,1,22);
 
 % Plot indian cases
@@ -19,12 +19,12 @@ ylabel('Cases Reported');
 title('India Study');
 
 % Make labels to days
-xticks(1:7:Ndays)
+xticks(0:7:Ndays)
 xticklabels(generateLabels(startDate, Ndays/7));
 
 % Exponential Fit
 isolatedData = indiaCases(1:numel(indiaCases));
-isolatedDays = 1:(numel(indiaCases));
+isolatedDays = 0:(numel(indiaCases)-1);
 model1 = fit(isolatedDays', isolatedData', 'exp1');
 
 % plot fitted curve and save 
