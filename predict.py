17043553b9ab1dfbfd2781ad17e22fd64a2ade67
Arjun Chauhan <arjunchau@gmail.com>
2020-03-16 10:03:58 +0530

    python codes

---
+++ predict.py
@@ -0,0 +1,31 @@
+import pandas as pd 
+import numpy as np 
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+file = pd.read_csv("time_series_19-covid-Confirmed.csv")
+
+file.set_index('Country/Region', inplace=True)
+cases = file.loc['India'][3:][:]
+cases = np.array(cases).astype(np.float64)
+days = (np.arange(cases.shape[0]).astype(np.float64))
+
+
+def logisitc(x, k, l, x0 = 0):
+    return (l/(1 + np.exp(-k*(x-x0))))
+
+def exponential(x, a , b, c):
+    return a * np.exp(-b * x) + c
+
+plt.plot(days, file.loc['India'][3:,],'o',color='blue', label = 'data')    
+
+#popt, pcov = curve_fit(exponential, days, cases)
+
+#logistic curve fitting
+popt, pcov = curve_fit(logisitc,days, cases)
+
+plt.plot(days, logisitc(days, *popt), 'r-',label='fit: k=%5.3f, l=%5.3f ,x0=%5.3f'% tuple(popt))
+plt.xlabel('Days')
+plt.ylabel('Cases in India')
+plt.legend()
+plt.savefig('py_curves.png')
+plt.show()
