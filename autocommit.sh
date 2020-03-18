293e5c4c046438deaa48f402dd7b5aab8cf23209
CommitBot <tri.utkarsh7596@gmail.com>
2020-03-18 08:40:21 +0000

    Log the outputs

--- autocommit.sh
+++ autocommit.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
+echo "" >> log.txt
+echo "" >> log.txt
 
-wall Starting commit
-git pull
-cd COVID-19
-git checkout master
-git pull
+date >> log.txt
+git pull >> log.txt
+cd COVID-19 
+git checkout master >> log.txt
+git pull >> log.txt
 cd ..
-python3 predict.py
-git add images
-git commit -m "CommitBot: Automatic commit"
-git push
-wall Done pushing
+python3 predict.py >> log.txt
+git add images >> log.txt
+git commit -m "CommitBot: Automatic commit" >> log.txt
+git push >> log.txt
+wall $(tail log.txt)
